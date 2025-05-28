5d8e3fce307c50d269553dd0c5ccca89fefc32d4
jake-bestland <beja0405@gmail.com>
2025-05-28 11:47:00 -0500

    finished lab

--- 04-conditionals_loops_functions/04.js
+++ 04-conditionals_loops_functions/04.js
@@ -21,14 +21,53 @@
  */
 
 // Write your code here:
+// #1
+function KtoC(tempInK) {
+    let tempInC = (tempInK - 273.15);
+    return tempInC;
+}
 
+function CtoK(tempInC) {
+    let tempInK = (tempInC+ 273.15);
+    return tempInK;
+}
 
+function FtoK(tempInF) {
+    let tempInK = ((tempInF - 32) * (5 / 9) + 273.15);
+    return tempInK;
+}
 
+function KtoF(tempInK) {
+    let tempInF = ((tempInK - 273.15) * (9 / 5) + 32);
+    return tempInF;
+}
 
+// #2
+function pythagoras(a, b) {
+    let cSquare = (a ** 2) + (b ** 2)
+    return Math.sqrt(cSquare);
+}
 
+// #3
+function inchToCm(inch) {
+    let cm = (inch * 2.54);
+    return cm;
+}
 
+function CmToInch(cm) {
+    let inch = (cm / 2.54);
+    return inch;
+}
 
+function MToKm(miles) {
+    let km = (miles * 1.60934);
+    return km;
+}
 
+function KmToM(km) {
+    let miles = (km / 1.60934);
+    return miles;
+}
 
 // Do not modify anything below this line
 
