5ade382be7d2d19ce5c107be739210f891680a47
jake-bestland <beja0405@gmail.com>
2025-06-18 10:41:02 -0500

    finished lab

--- 06-advanced_arrays_strings_functions/01.js
+++ 06-advanced_arrays_strings_functions/01.js
@@ -9,3 +9,70 @@
  * 5. Refactor: define the sum, count and average function as a separate
  *    function and pass it to the forEach functions as a name.
  */
+
+// #1
+function getRandomNumber(){
+    return Math.floor((Math.random() * 100) + 1);
+}
+
+let randomArray = [];
+let randomNumber = getRandomNumber();
+for (let i = 1; i <= 100; i++) {
+    randomArray.push(randomNumber);
+    randomNumber = getRandomNumber();
+}
+console.log(randomArray)
+
+// #2
+let sum = 0;
+let count = 0;
+let avg = 0;
+randomArray.forEach((number) => {
+    sum = number + sum;
+    count += 1;
+    avg = sum / count;
+});
+console.log(`The sum of the randomArray is ${sum}, the count is ${count}, and the average is ${avg}`)
+
+// #3
+let halfArray = []
+randomArray.forEach((number) => {
+    halfArray.push(number / 2);
+});
+console.log(halfArray)
+
+// #4
+sum = 0;
+count = 0;
+avg = 0;
+halfArray.forEach((number) => {
+    sum = number + sum;
+    count += 1;
+    avg = sum / count;
+});
+console.log(`The sum of the halfArray is ${sum}, the count is ${count}, and the average is ${avg}`)
+
+// #5
+function sumNumber(a, b) {
+    let result = a + b;
+    return result;
+}
+
+function countArray(array) {
+    count = array.length;
+    return count;
+}
+
+function avgNumber(sum, count) {
+    avg = sum / count;
+    return avg;
+}
+sum = 0;
+count = 0;
+avg = 0;
+halfArray.forEach((number) => {
+    sum = sumNumber(sum, number);
+    count = countArray(halfArray);
+    avg = avgNumber(sum, count)
+});
+console.log(`The sum of the halfArray is ${sum}, the count is ${count}, and the average is ${avg}`)
