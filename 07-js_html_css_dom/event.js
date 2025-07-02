1fadc0c0ab908cbb1b8f4732cc0407d6047bc343
jake-bestland <beja0405@gmail.com>
2025-07-02 13:04:46 -0500

    Added buttons to increase and decrease font size

--- 07-js_html_css_dom/event.js
+++ 07-js_html_css_dom/event.js
@@ -2,5 +2,24 @@ function toggleDarkMode() {
     let bod = document.body;
     bod.classList.toggle("dark-mode");
 }
+
+function increaseTextSize() {
+    let currentFontSize = parseFloat(window.getComputedStyle(document.body).fontSize);
+    let newFontSize = (currentFontSize / 16) + 0.1;
+    document.body.style.fontSize = `${newFontSize}rem`;
+}
+
+function decreaseTextSize() {
+    let currentFontSize = parseFloat(window.getComputedStyle(document.body).fontSize);
+    let newFontSize = (currentFontSize / 16) - 0.1;
+    document.body.style.fontSize = `${newFontSize}rem`;
+}
+
 let btn = document.getElementById("theme-switch");
 btn.addEventListener("click", toggleDarkMode);
+
+let increaseButton = document.getElementById("increase-font");
+increaseButton.addEventListener("click", increaseTextSize);
+
+let decreaseButton = document.getElementById("decrease-font");
+decreaseButton.addEventListener("click", decreaseTextSize);
